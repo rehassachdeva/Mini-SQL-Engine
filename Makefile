cc5a1c025dd2973dd944c4971150a4955b2e72de
Rehas Sachdeva <aquannie@gmail.com>
2016-09-04 01:32:52 +0530

    Add select *, col from single table, aggregate queries

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-CFLAGS = -std=c++11 -lstdc++ -Wall -I./libs/sqlparser/ -L./libs/
+CFLAGS = -std=c++11 -lstdc++ -I./libs/sqlparser/ -L./libs/
 
 all:
 	$(CXX) $(CFLAGS) minisql.cpp -o minisql -lsqlparser
--- minisql.cpp
+++ minisql.cpp
@@ -11,73 +11,208 @@ using namespace std;
 #define RUN_TABLE 1
 #define ENDED_TABLE 2
 
+#define AGGMAX "max"
+#define AGGMIN "min"
+#define AGGAVG "avg"
+#define AGGSUM "sum"
+
 int numTables=0;
 map<string, vector<string> > tables;
 
 void parseMetadata() {
-	ifstream inFile("metadata.txt");
-	string line,curTable;
-	int flag=ENDED_TABLE;
-	while(inFile>>line) {
-		if(line=="<begin_table>") {
-			flag=BEGAN_TABLE;
-			numTables++;
-		}
-		else if(line=="<end_table>") {
-			flag=ENDED_TABLE;
-		}
-		else if(flag==BEGAN_TABLE) {
-			vector<string> attributes;
-			string newTable=line;
-			tables[newTable]=attributes;
-			curTable=line;
-			flag=RUN_TABLE;
-		}
-		else {
-			string newAttr=line;
-			tables[curTable].push_back(newAttr);
-		}
-	}
-	inFile.close();
+    ifstream inFile("metadata.txt");
+    string line,curTable;
+    int flag=ENDED_TABLE;
+    while(inFile>>line) {
+        if(line=="<begin_table>") {
+            flag=BEGAN_TABLE;
+            numTables++;
+        }
+        else if(line=="<end_table>") {
+            flag=ENDED_TABLE;
+        }
+        else if(flag==BEGAN_TABLE) {
+            vector<string> attributes;
+            string newTable=line;
+            tables[newTable]=attributes;
+            curTable=line;
+            flag=RUN_TABLE;
+        }
+        else {
+            string newAttr=line;
+            tables[curTable].push_back(newAttr);
+        }
+    }
+    inFile.close();
 }
 
-void printTables() {
-	map<string, vector<string> >::iterator it;
-	for(it=tables.begin();it!=tables.end();it++) {
-		cout<<(*it).first<<endl;
-		vector<string>::iterator it2;
-		for(it2=(*it).second.begin();it2!=(*it).second.end();it2++) {
-			cout<<*it2<<" ";
-		}
-		cout<<endl;
-	}
+vector<string> split(const string &s, char delim) {
+    stringstream ss(s);
+    string item;
+    vector<string> tokens;
+    while (getline(ss, item, delim)) {
+        tokens.push_back(item);
+    }
+    return tokens;
 }
 
 int main(int argc, char *argv[]) {
-	/*if(argc<=0) {
-        fprintf(stderr,"Usage: ./minisql \"SELECT * FROM test;\"\n");
+    if(argc<=1) {
+        cout<<"Usage: ./minisql \"SELECT * FROM test;\"\n";
         return -1;
     }
-    string query=argv[1];
+    const string input=argv[1];
 
-    hsql::SQLParserResult* result=hsql::SQLParser::parseSQLString(query);
+    vector<string> queries=split(input, ';');
 
-    if(result->isValid) {
-        printf("Parsed successfully!\n");
-        printf("Number of statements: %lu\n", result->size());
+    for(string query : queries) {
 
-        for (hsql::SQLStatement* stmt : result->statements) {
-            hsql::printStatementInfo(stmt);
+        hsql::SQLParserResult* result=hsql::SQLParser::parseSQLString(query);
+
+        if(!result->isValid) {
+            cout<<"Invalid SQL!\n";
+            cout<<result->errorMsg<<endl;
+            cout<<"Error line: "<<result->errorLine+1<<endl;
+            cout<<"Error column: "<<result->errorColumn+1<<endl;
+            continue;
         }
-        return 0;
-    } 
-    else {
-        printf("Invalid SQL!\n");
-        return -1;
-    }*/
 
-    parseMetadata();
-    printTables(); 
+        hsql::SQLStatement* stmt=result->getStatement(0);
 
-	return 0;
+        if(stmt->type()!=hsql::kStmtSelect) {
+            cout<<"Sorry! Query \""<<query<<"\" is out of scope!\n";
+            continue;
+        }
+
+        parseMetadata();
+
+        hsql::SelectStatement* selectStmt=(hsql::SelectStatement*)stmt;
+        hsql::TableRef* table=selectStmt->fromTable;
+
+        if(table->type==hsql::kTableName and tables.find(table->name)==tables.end()) {
+            cout<<"Table "<<table->name<<" not found!\n";
+            continue;
+        }
+
+        bool notFound=false;
+
+        if(table->type==hsql::kTableCrossProduct) {
+            for (hsql::TableRef* tbl : *table->list) {
+                if(tables.find(tbl->name)==tables.end()) {
+                    cout<<"Table "<<tbl->name<<" not found!\n";
+                    notFound=true;
+                }
+            }
+            if(notFound==true)
+                continue;
+        }
+
+        if(result->isValid) {
+            for (hsql::SQLStatement* stmt : result->statements) {
+                hsql::printStatementInfo(stmt);
+            }
+        }
+
+        if(table->type==hsql::kTableName and 
+                (*selectStmt->selectList).size()==1 and 
+                (*selectStmt->selectList)[0]->type==hsql::kExprStar) {
+
+            ifstream tableFile((string)table->name+".csv");
+            int i=0;
+            string line;
+            for(i=0;i<tables[table->name].size()-1;i++)
+                cout<<table->name<<"."<<tables[table->name][i]<<",";
+            cout<<table->name<<"."<<tables[table->name][i]<<endl;
+
+            while(!tableFile.eof()) {
+                tableFile>>line;
+                cout<<line<<endl;
+            }
+
+            continue;
+        }
+
+        if(table->type==hsql::kTableName and
+                (*selectStmt->selectList).size()==1 and
+                (*selectStmt->selectList)[0]->type==hsql::kExprFunctionRef) {
+
+            string attr=(*selectStmt->selectList)[0]->expr->name;
+            vector<string>::iterator it;
+            it=find(tables[table->name].begin(),tables[table->name].end(),attr);
+
+            if(it==tables[table->name].end()) {
+                cout<<"Column "<<attr<<" not found!\n";
+                continue;
+            }
+
+            int attrNum=it-tables[table->name].begin();
+
+            string line;
+
+            ifstream tableFile((string)table->name+".csv");
+
+            long long sum=0,numLines=0,mx=LLONG_MIN,mn=LLONG_MAX;
+
+            string aggType=(*selectStmt->selectList)[0]->name;
+
+            transform(aggType.begin(), aggType.end(), aggType.begin(),
+                    [](unsigned char c) { return std::tolower(c); });
+
+            if(aggType!=AGGMAX and aggType!=AGGMIN and aggType!=AGGAVG and aggType!=AGGSUM) {
+                cout<<"Aggregate Function "<<aggType<<" not found!\n";
+                cout<<"Available aggregate functions are max, min, avg and sum\n";
+                continue;
+            }
+
+            while(!tableFile.eof()) {
+                tableFile>>line;
+                numLines++;
+                vector<string> items=split(line,',');
+
+                mx=max(stoll(items[attrNum]),mx);
+                mn=min(stoll(items[attrNum]),mn);
+                sum+=stoll(items[attrNum]);
+            }
+
+            if(aggType==AGGMAX) cout<<mx<<endl;
+            else if(aggType==AGGMIN) cout<<mn<<endl;
+            else if(aggType==AGGAVG) cout<<(double)sum/numLines<<endl;
+            else if(aggType==AGGSUM) cout<<sum<<endl;
+
+            continue;
+        }
+
+        notFound=false;
+        if(table->type==hsql::kTableName) {
+            vector<hsql::Expr*> attributes=(*selectStmt->selectList);
+            vector<int> cols;
+            for(int i=0;i<attributes.size();i++) {
+                vector<string>::iterator it;
+                it=find(tables[table->name].begin(),tables[table->name].end(),attributes[i]->name);
+                if(it==tables[table->name].end()) {
+                    cout<<"Column "<<attributes[i]->name<<" not found!\n";
+                    notFound=true;
+                    break;
+                }
+                else
+                    cols.push_back(it-tables[table->name].begin());
+            }
+            if(notFound) continue;
+
+            ifstream tableFile((string)table->name+".csv");
+            string line;
+
+            while(!tableFile.eof()) {
+                tableFile>>line;
+                vector<string> items=split(line,',');
+                int i;
+                for(i=0;i<cols.size()-1;i++) {
+                    cout<<items[cols[i]]<<",";
+                }    		
+                cout<<items[cols[i]]<<endl;
+            }
+        }
+    }    
+    return 0;
 }
+
